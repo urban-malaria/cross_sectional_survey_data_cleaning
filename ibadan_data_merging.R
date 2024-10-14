5f0f54d7108bafcb4b5bd84f3a91cd2345450d1e
Laurette Mhlanga <laurettemhlanga13@gmail.com>
2024-10-14 13:44:47 -0500

    creating dictionaries and saving the files as .dta

--- ibadan_data_merging.R
+++ ibadan_data_merging.R
@@ -32,6 +32,29 @@ wide_ib_household_travelers <- ib_household_travelers %>%
               values_from = c(c1iii:tfhi_complete))
 
 
+stata_format <- function(name) {
+ 
+  name <- gsub("[\\(\\)]", "", name)  # Remove parentheses
+  name <- gsub("[^[:alnum:]_]", "_", name)  # Replace special characters with underscores
+  name <- tolower(name)  # Convert to lowercase
+  
+  # Ensure names are no longer than 32 characters
+  name <- substr(name, 1, 32)
+  
+  # Make sure the name starts with a letter or underscore
+  if (!grepl("^[a-zA-Z_]", name)) {
+    name <- paste0("v_", name)  
+  }
+  
+    return(name)
+}
+  
+  
+names(wide_ib_household_travelers) <- sapply(names(wide_ib_household_travelers), stata_format)
+
+
+
+
 wide_ib_household_net_insp <- ib_household_net_insp %>%
   select(-c(redcap_repeat_instrument)) %>% 
   pivot_wider(names_from = c(redcap_repeat_instance), 
@@ -45,7 +68,16 @@ wide_data_together <- left_join(wide_Ibadan_data, ib_household) %>%
   left_join(wide_ib_household_travelers, by = ("sn")) %>% 
   left_join(wide_ib_household_visitors, by = ("sn"))
 
-write_dta(wide_data_together, "C:/Users/laure/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/kano_ibadan_epi/new_field_data/240922_Ibadan_latest_data/all_ibadan_wetseason_dater_wide.dta")  
+#cleaning names for saving 
+wide_data_together_filtered <- wide_data_together[, !grepl("\\.y", names(wide_data_together))]
+
+names(wide_data_together_filtered)[grepl("\\.x", names(wide_data_together_filtered))] <- gsub("\\.x", "", names(wide_data_together_filtered)[grepl("\\.x", names(wide_data_together_filtered))])
+
+names(wide_data_together_filtered) <- gsub("[^[:alnum:]]", "_", names(wide_data_together_filtered))
+
+
+
+# write_dta(wide_data_together_filtered, "C:/Users/laure/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/kano_ibadan_epi/new_field_data/240922_Ibadan_latest_data/all_ibadan_wetseason_dater_wide.dta")  
 
 ##################################
 #long data 
@@ -138,7 +170,19 @@ names(ib_men_survey) <- c("sn", paste0("men_", names(ib_men_survey)[-1]))
 
 
 
-long_data_together <- left_join(Ibadan_household_list, ib_household, by = ("sn")) %>% 
+ib_household_common_names <- which(names(ib_household) %in% allnames_householdlist)[-1]
+
+ib_household_mod <- ib_household %>% 
+  dplyr::select(-c(ib_household_common_names))
+
+ib_household <- ib_household%>% 
+  dplyr::select(-c(redcap_repeat_instrument, 
+         redcap_repeat_instance, hl4, ))
+
+
+
+
+long_data_together <- left_join(Ibadan_household_list, ib_household_mod, by = ("sn")) %>% 
   dplyr::left_join(ib_men_survey, by = ("sn")) %>% 
   left_join(ib_women_survey, by = ("sn" )) %>% 
   left_join(ib_household_net_insp_mod_hh, by = ("sn")) %>% 
@@ -146,7 +190,53 @@ long_data_together <- left_join(Ibadan_household_list, ib_household, by = ("sn")
   left_join(ib_household_visitors_mod, by = ("sn"))
 
 
+
+
+# write_dta(long_data_together, "C:/Users/laure/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/kano_ibadan_epi/new_field_data/240922_Ibadan_latest_data/ibadan_long_wetseason_household_members.dta")  
+# 
+# 
+# write.csv(names(long_data_together), "C:/Users/laure/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/kano_ibadan_epi/new_field_data/240922_Ibadan_latest_data/dictionary_ibadan_long_wetseason_household_members.csv")
+# 
+
+
+
+
+
+
+
+extract_labels <- function(df) {
+
+  column_names <- names(df)
   
+  column_labels <- sapply(df, function(x) {
+    # Handle multiple cases, ensuring consistency
+    label <- attr(x, "label")
+    if (is.null(label)) {
+      return(NA)  
+    } else if (length(label) > 1) {
+  
+      return(paste(label, collapse = ", "))
+    } else {
+      return(label)  
+    }
+  })
+  
+  
+  column_labels[is.na(column_labels)] <- column_names[is.na(column_labels)]
+  
+  
+  label_df <- data.frame(ColumnName = column_names, Label = column_labels, stringsAsFactors = FALSE)
+  
+  return(label_df)
+}
 
 
+labels_df <- extract_labels(wide_data_together_filtered)
+long_labels_df <- extract_labels(long_data_together)
+
+
+# Write to a CSV file
+# write.csv(labels_df, "C:/Users/laure/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/kano_ibadan_epi/new_field_data/240922_Ibadan_latest_data/dictionary_ibadan_wide_wetseason_household_members.csv", row.names = FALSE)
+# 
+# write.csv(long_labels_df, "C:/Users/laure/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/kano_ibadan_epi/new_field_data/240922_Ibadan_latest_data/dictionary_ibadan_long_wetseason_household_members.csv", row.names = FALSE)
 
